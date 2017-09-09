b446c3c2cdf319e14119ba3ff6321f2dc1cf270c
dthoma6403 <dthoma6403@gmail.com>
2017-09-08 21:08:31 -0400

    Added My Unity Project

---
+++ Doors/Assets/Doors.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour 
+{
+	public bool startRotationFlag;
+	public Transform target;
+	public float speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+	void Update()
+	{
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+
+		if(startRotationFlag){
+			this.transform.Rotate ( new Vector3(45,0,0) * Time.deltaTime );
+							}
+	}
+
+	void OnMouseUp()
+	{
+		startRotationFlag = true;
+	}
+
+}
+
